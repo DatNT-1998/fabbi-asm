d93fdfecce77c04a799f94238b9e76dd5fbff31f
DatNT1998 <44127716+DatNT1998@users.noreply.github.com>
2020-09-29 14:44:20 +0700

    Updates

---
+++ precache-manifest.8cc6903878813dbd2856ae990d3abe49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac0e3ff0edd7cbeb0b97342fd738c6b",
+    "url": "/fabbi-asm/index.html"
+  },
+  {
+    "revision": "eb7568bf11dd8a4818fd",
+    "url": "/fabbi-asm/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "6eee9bc82ef525fdfaea",
+    "url": "/fabbi-asm/static/css/main.643a9fab.chunk.css"
+  },
+  {
+    "revision": "eb7568bf11dd8a4818fd",
+    "url": "/fabbi-asm/static/js/2.5dd7d361.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fabbi-asm/static/js/2.5dd7d361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eee9bc82ef525fdfaea",
+    "url": "/fabbi-asm/static/js/main.e3e1c638.chunk.js"
+  },
+  {
+    "revision": "6f8c76124e5594bca79c",
+    "url": "/fabbi-asm/static/js/runtime-main.3fd6f9c7.js"
+  }
+]);
